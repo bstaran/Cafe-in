255a5beb4eff4ccb93e210f59ba7e54bbc886597
BaikSeunghan <tmdgks103@naver.com>
2022-07-23 17:50:03 +0900

    feat : 게시글 등록 및 전체 조회 기능 구현

--- cafe-in/src/main/java/com/team2/cafein/CafeInApplication.java
+++ cafe-in/src/main/java/com/team2/cafein/CafeInApplication.java
@@ -2,8 +2,10 @@ package com.team2.cafein;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CafeInApplication {
 
     public static void main(String[] args) {
