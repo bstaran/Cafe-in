1b73a926ccaa074c9b4ac4baa4935938c8a87717
fox9d <devafox62@gmail.com>
2022-07-23 10:12:48 +0900

    add User Controller, Table, Repository

---
+++ cafe-in/src/main/java/com/team2/cafein/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.team2.cafein.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
---
+++ cafe-in/src/main/java/com/team2/cafein/model/User.java
@@ -0,0 +1,26 @@
+package com.team2.cafein.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
