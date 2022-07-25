f334ecd1316eaf5ff2043d301e6bce3dcea34ce6
Tmoney2014 <107834618+Tmoney2014@users.noreply.github.com>
2022-07-25 14:29:46 +0900

    필요없는 임포트구문 및 필요없는 주석 제거

--- src/main/java/com/team2/cafein/controller/BookmarkController.java
+++ src/main/java/com/team2/cafein/controller/BookmarkController.java
@@ -2,12 +2,14 @@ package com.team2.cafein.controller;
 
 
 import com.team2.cafein.dto.ResponseMessageDto;
-import com.team2.cafein.model.Bookmark;
+import com.team2.cafein.model.Post;
 import com.team2.cafein.service.BookmarkService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -20,43 +22,12 @@ public class BookmarkController {
    public List<Post> getBookmarkedPost(@PathVariable Long userId){
 
         return bookmarkService.getPosts(userId); //서비스의 getPosts 실행 결과값을 리턴으로 받는다. (List<post>) 의형식으로
-
-
-//        // 로그인 되어있는 userId로 Bookmark 테이블에서 select로 리스트 배열 받아오기
-//        List<Bookmark> bookmarks = bookmarkService.getPosts(userId);
-//
-//        List<Post> responsePosts = new ArrayList<>();   // 여기는 응답할 게시글 목록을 위한 리스트 선언
-//
-//        for (Bookmark bookmark : bookmarks) {
-//            Long postId = bookmark.getPostId();
-//            Post post = postRepository.findByPostId(postId);
-//            responsePosts.add(post);
-//        }
-
-//        for (int i = 0; i < posts.size(); i++) {
-//            Bookmark bookmark = posts.get(i);
-//            responsePosts.add(post);
-//        }
     }
 
     @GetMapping("/api/bookmark/{postId}") // 유저 아이디 어디서 갖고오냐 ...
     public ResponseMessageDto savePost(@PathVariable Long postId,Long userId){
         return bookmarkService.savePost(userId,postId);
     }
-
-//    // 댓글 작성
-//    @PostMapping("/api/reply")
-//    public String createReply(@RequestBody ReplyRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-//        // 로그인 되어 있는 ID
-//        if (userDetails != null) {
-//            Long userId = userDetails.getUser().getId();
-//            String username = userDetails.getUser().getUsername();
-//            ReplyService.createReply(requestDto, username, userId);
-//            return "댓글 작성 완료";
-//        }
-//        return "로그인이 필요한 기능입니다.";
-//    }
-
 
     @DeleteMapping("/api/bookmark/{bookmarkId}")
     public ResponseMessageDto deleteBookmark(@PathVariable Long bookmarkId)  {
