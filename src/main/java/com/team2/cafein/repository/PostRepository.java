78dd2457f1e2a69da2eaa49313ac2e549df4ef52
BaikSeunghan <tmdgks103@naver.com>
2022-07-27 09:30:09 +0900

    refactor : 파일 업로드 설정(3)

--- src/main/java/com/team2/cafein/repository/PostRepository.java
+++ src/main/java/com/team2/cafein/repository/PostRepository.java
@@ -12,4 +12,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByUser(User user);
 
     Optional<Post> findById(Long postId);
+
+    List<Post> findAllByOrderByCreatedAtDesc();
 }
