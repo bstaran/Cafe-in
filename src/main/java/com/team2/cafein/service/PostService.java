78ba17d2b8fa095727fc85ff44c9df0d39f55df7
BaikSeunghan <tmdgks103@naver.com>
2022-07-28 15:28:41 +0900

    refactor : 파일 업로드 설정(4)-1

--- src/main/java/com/team2/cafein/service/PostService.java
+++ src/main/java/com/team2/cafein/service/PostService.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 
